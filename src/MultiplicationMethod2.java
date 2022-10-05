6fd0f048b064e8f922748f0dd1cb3a3fa989e14c
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-10-05 22:07:43 +0900

    2022-10-05 today's work uploaded

---
+++ src/MultiplicationMethod2.java
@@ -0,0 +1,24 @@
+
+public class MultiplicationMethod2 {
+	
+	public static int[] calculate(int times) {
+		int[] result = new int[9];
+			for(int i=0; i<result.length; i++) {
+				result[i] = times*(i+1);
+			}
+			return result;
+	}
+	
+	public static void print(int[] result) {
+		for(int i=0; i<result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main(String[]args) {
+		for(int i=2; i<10; i++){
+			int[] result = calculate(i);
+			print(result);
+		}
+	}
+}
