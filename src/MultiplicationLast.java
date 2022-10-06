bd7d2bc6f76898e0054fa451fa90eb13336420fa
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-10-06 23:54:58 +0900

    convert from string to int

---
+++ src/MultiplicationLast.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MultiplicationLast {
+
+	public static void main(String[]args) {
+		Scanner scanner = new Scanner(System.in);
+		String inputValue = scanner.nextLine();
+		
+		String[] splitedValue = inputValue.split(",");
+		
+		int first = Integer.parseInt(splitedValue[0]);
+		int second = Integer.parseInt(splitedValue[1]);
+		
+		for(int i=1; i<first.length; i++) {
+			for
+		}
+			
+	}
+}
