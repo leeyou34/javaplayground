143f4cd9267a5e27280cdc828df23e5d7a123ddc
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-10-08 16:59:41 +0900

    do while statement

---
+++ src/MultiplicationLast4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MultiplicationLast4 {
+	public static void main(String[]args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		String inputValue = scanner.nextLine();
+		
+		String[]splitedValue = inputValue.split(",");
+		
+		int first=Integer.parseInt(splitedValue[0]);
+		int second=Integer.parseInt(splitedValue[1]);
+		
+		int i=2;
+		do {
+			int j=1;
+			do {
+				System.out.println(i+"x"+j+"="+i*j);
+			} while (j++ <second);
+		} while (i++ < first);
+	}
+}
