dc1ba322abe4273ed50e8234661f8d8a15757056
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-10-08 17:24:00 +0900

    method creationg practice again...

---
+++ src/MultiplicationLast5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MultiplicationLast5 {
+//practice method creation...
+	public static int[] calculate(int times) {
+		int[] result = new int[9];
+			for(int i=0; i<result.length; i++) {
+				result[i] =times*(i+1);
+			} return result;
+	}
+	
+	public static void print(int[] result) {
+		for(int i=0; i<result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main(String[]args) {
+		for(int i=2; i<10; i++) {
+			int[] result = calculate(i);
+			print(result);
+		}
+	}
+}
